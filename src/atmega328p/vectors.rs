b41d6bca2174940edbf568b15c946c62bc295a7b
Ruslan Curbanov <rucub100@hhu.de>
2024-07-29 20:01:26 +0200

    finish interrupt vectors

--- src/atmega328p/vectors.rs
+++ src/atmega328p/vectors.rs
@@ -1,7 +1,9 @@
 /// Interrupt Vectors in ATmega328P
+/// See page 49 in ATmega328P datasheet
 
+/// Reset + 25 Interrupt Vectors
 #[allow(dead_code)]
-const IVT_SIZE: usize = 0x1a;
+const IVT_SIZE: usize = 26;
 
 // /// INT0 - External Interrupt Request 0
 // #[no_mangle]
@@ -87,9 +89,93 @@ const IVT_SIZE: usize = 0x1a;
 //     // Your ISR code here
 // }
 
-/// TIMER1 OVF - Timer/Counter1 Overflow
-#[no_mangle]
-#[export_name = "__vector_13"]
-pub extern "avr-interrupt" fn timer1_ovf_isr() {
-    // Your ISR code here
-}
+// /// TIMER1 OVF - Timer/Counter1 Overflow
+// #[no_mangle]
+// #[export_name = "__vector_13"]
+// pub extern "avr-interrupt" fn timer1_ovf_isr() {
+//     // Your ISR code here
+// }
+
+// /// TIMER0 COMPA - Timer/Counter0 Compare Match A
+// #[no_mangle]
+// #[export_name = "__vector_14"]
+// pub extern "avr-interrupt" fn timer0_compa_isr() {
+//     // Your ISR code here
+// }
+
+// /// TIMER0 COMPB - Timer/Counter0 Compare Match B
+// #[no_mangle]
+// #[export_name = "__vector_15"]
+// pub extern "avr-interrupt" fn timer0_compb_isr() {
+//     // Your ISR code here
+// }
+
+// /// TIMER0 OVF - Timer/Counter0 Overflow
+// #[no_mangle]
+// #[export_name = "__vector_16"]
+// pub extern "avr-interrupt" fn timer0_ovf_isr() {
+//     // Your ISR code here
+// }
+
+// /// SPI, STC - SPI Serial Transfer Complete
+// #[no_mangle]
+// #[export_name = "__vector_17"]
+// pub extern "avr-interrupt" fn spi_stc_isr() {
+//     // Your ISR code here
+// }
+
+// /// USART, RX - USART Rx Complete
+// #[no_mangle]
+// #[export_name = "__vector_18"]
+// pub extern "avr-interrupt" fn usart_rx_isr() {
+//     // Your ISR code here
+// }
+
+// /// USART, UDRE - USART Data Register Empty
+// #[no_mangle]
+// #[export_name = "__vector_19"]
+// pub extern "avr-interrupt" fn usart_udre_isr() {
+//     // Your ISR code here
+// }
+
+// /// USART, TX - USART Tx Complete
+// #[no_mangle]
+// #[export_name = "__vector_20"]
+// pub extern "avr-interrupt" fn usart_tx_isr() {
+//     // Your ISR code here
+// }
+
+// /// ADC - ADC Conversion Complete
+// #[no_mangle]
+// #[export_name = "__vector_21"]
+// pub extern "avr-interrupt" fn adc_isr() {
+//     // Your ISR code here
+// }
+
+// /// EE READY - EEPROM Ready
+// #[no_mangle]
+// #[export_name = "__vector_22"]
+// pub extern "avr-interrupt" fn ee_ready_isr() {
+//     // Your ISR code here
+// }
+
+// /// ANALOG COMP - Analog Comparator
+// #[no_mangle]
+// #[export_name = "__vector_23"]
+// pub extern "avr-interrupt" fn analog_comp_isr() {
+//     // Your ISR code here
+// }
+
+// /// TWI - 2-wire Serial Interface (I2C)
+// #[no_mangle]
+// #[export_name = "__vector_24"]
+// pub extern "avr-interrupt" fn twi_isr() {
+//     // Your ISR code here
+// }
+
+// /// SPM READY - Store Program Memory Ready
+// #[no_mangle]
+// #[export_name = "__vector_25"]
+// pub extern "avr-interrupt" fn spm_ready_isr() {
+//     // Your ISR code here
+// }
