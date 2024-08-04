f4d1119a20b8b3634f6b03d10cd4b301473c026a
Ruslan Curbanov <rucub100@hhu.de>
2024-08-04 22:19:49 +0200

    interrupts: enable/disable global interrupts

--- src/atmega328p/interrupts.rs
+++ src/atmega328p/interrupts.rs
@@ -1,2 +1,18 @@
 //! Interrupts
 //! Datasheet page 49 et seqq.
+
+use core::arch::asm;
+
+#[inline(always)]
+pub(super) fn enable_global_interrupts() {
+    unsafe {
+        asm!("sei");
+    }
+}
+
+#[inline(always)]
+pub(super) fn disable_global_interrupts() {
+    unsafe {
+        asm!("cli");
+    }
+}
