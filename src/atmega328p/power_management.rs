9ae125783f9bbfdf4a49dd701616a5ef67c1e636
Ruslan Curbanov <rucub100@hhu.de>
2024-08-06 15:37:48 +0200

    power reduction

--- src/atmega328p/power_management.rs
+++ src/atmega328p/power_management.rs
@@ -1,11 +1,20 @@
 //! Power Management and Sleep Modes
 //! Datasheet page 34 et seqq.
 
-use core::{arch::asm, ptr::write_volatile};
+use core::{
+    arch::asm,
+    ptr::{read_volatile, write_volatile},
+};
 
 use crate::atmega328p::interrupts;
 
-use super::registers::{BODS, BODSE, MCUCR_ADDR_IO, SMCR_ADDR};
+use super::{
+    interrupts::free,
+    registers::{
+        BODS, BODSE, MCUCR_ADDR_IO, PRADC, PRR_ADDR, PRSPI, PRTIM0, PRTIM1, PRTIM2, PRTWI, PRUSAR0,
+        SMCR_ADDR,
+    },
+};
 
 #[allow(dead_code)]
 pub enum SleepMode {
@@ -53,6 +62,62 @@ pub fn sleep_mode_with_brown_out_detector_disabled(mode: SleepMode) {
         });
         asm!("sleep");
     }
+}
+
+#[allow(dead_code)]
+pub fn poweroff_i2c() {
+    unsafe {
+        prr_set_bit_interrupts_free(PRTWI);
+    }
+}
+
+#[allow(dead_code)]
+pub fn poweroff_timer0() {
+    unsafe {
+        prr_set_bit_interrupts_free(PRTIM0);
+    }
+}
+
+#[allow(dead_code)]
+pub fn poweroff_timer1() {
+    unsafe {
+        prr_set_bit_interrupts_free(PRTIM1);
+    }
+}
+
+#[allow(dead_code)]
+pub fn poweroff_timer2() {
+    unsafe {
+        prr_set_bit_interrupts_free(PRTIM2);
+    }
+}
+
+#[allow(dead_code)]
+pub fn poweroff_adc() {
+    unsafe {
+        prr_set_bit_interrupts_free(PRADC);
+    }
+}
+
+#[allow(dead_code)]
+pub fn poweroff_spi() {
+    unsafe {
+        prr_set_bit_interrupts_free(PRSPI);
+    }
+}
+
+#[allow(dead_code)]
+pub fn poweroff_usart() {
+    unsafe {
+        prr_set_bit_interrupts_free(PRUSAR0);
+    }
+}
+
+unsafe fn prr_set_bit_interrupts_free(bit: u8) {
+    interrupts::free(|| {
+        let prr = read_volatile(PRR_ADDR as *const u8);
+        write_volatile(PRR_ADDR as *mut u8, prr | 1 << bit);
+    });
 }
 
 #[inline(always)]
